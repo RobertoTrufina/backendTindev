de8efd17ea5631a1dcb5f33c9dcce4e5b192fed4
Roberto Pinto <robertotrufina@gmail.com>
2022-07-13 10:21:10 -0300

    conclusion backend add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const server = express()
 
@@ -8,8 +9,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.ac1dr.mongodb.net/o
     useNewUrlParser: true
 })
 
+server.use(cors())
 server.use(express.json())
-
 server.use(routes)
 
 server.listen(3333)
